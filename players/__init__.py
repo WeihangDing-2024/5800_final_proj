ecfd17ee53a32b3cb7b8a272c560dac6aa425304
weihang <may971119@gmail.com>
2026-02-22 17:22:27 -0800

    add gui using tkinter

--- players/__init__.py
+++ players/__init__.py
@@ -7,9 +7,11 @@ Provides base classes and concrete implementations for different player types.
 from .base import Player
 from .terminal_player import TerminalPlayer
 from .subprocess_player import SubprocessPlayer
+from .gui_player import GUIPlayer
 
 __all__ = [
     'Player',
     'TerminalPlayer',
     'SubprocessPlayer',
+    'GUIPlayer',
 ]
